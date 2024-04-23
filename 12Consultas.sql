0d5ac85c934d1ea3457ce2cf273f45fbb459b69d
RafaelBP02 <rafaelbertopereira@gmail.com>
2024-04-23 17:52:17 -0300

    Feat: Cria consulta 3

--- 12Consultas.sql
+++ 12Consultas.sql
@@ -18,3 +18,9 @@ SELECT Nome, Ano FROM Filmes
 -- Buscar o nome e ano dos filmes, ordenados por ordem crescente pelo ano
 SELECT Nome, Ano FROM Filmes
 ORDER BY Ano 
+
+
+-- Consulta 3
+--Buscar pelo filme de volta para o futuro, trazendo o nome, ano e a duração
+SELECT Nome, Ano, Duracao FROM Filmes
+WHERE Nome = 'De Volta para o Futuro'
