97c16a88be6098405556854a048ac9879fcc8472
RafaelBP02 <rafaelbertopereira@gmail.com>
2024-04-23 17:59:49 -0300

    Feat: Cria consulta 6

--- 12Consultas.sql
+++ 12Consultas.sql
@@ -37,3 +37,11 @@ WHERE Ano = 1997
 SELECT Nome, Ano, Duracao FROM Filmes
 WHERE Ano > 2000
 ORDER BY Ano
+
+
+-- Consulta 6
+-- Buscar os filmes com a duracao maior que 100 e menor que 150, 
+-- ordenando pela duracao em ordem crescente
+SELECT Nome, Ano, Duracao FROM Filmes
+WHERE Duracao > 100 AND Duracao < 150
+ORDER BY Duracao
