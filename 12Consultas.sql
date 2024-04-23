042e2347f4395efdffc440911b7ecdb9c054feb9
RafaelBP02 <rafaelbertopereira@gmail.com>
2024-04-23 18:19:03 -0300

    Feat: Cria consulta 10 e muda formatacao do script

--- 12Consultas.sql
+++ 12Consultas.sql
@@ -11,30 +11,35 @@
 
 -- Consulta 1 
 -- Buscar o nome e ano dos filmes
-SELECT Nome, Ano FROM Filmes
+SELECT Nome, Ano 
+FROM Filmes
 
 
 -- Consulta 2
 -- Buscar o nome e ano dos filmes, ordenados por ordem crescente pelo ano
-SELECT Nome, Ano FROM Filmes
+SELECT Nome, Ano 
+FROM Filmes
 ORDER BY Ano 
 
 
 -- Consulta 3
 -- Buscar pelo filme de volta para o futuro, trazendo o nome, ano e a duração
-SELECT Nome, Ano, Duracao FROM Filmes
+SELECT Nome, Ano, Duracao 
+FROM Filmes
 WHERE Nome = 'De Volta para o Futuro'
 
 
 -- Consulta 4
 -- Buscar os filmes lançados em 1997
-SELECT Nome, Ano, Duracao FROM Filmes
+SELECT Nome, Ano, Duracao 
+FROM Filmes
 WHERE Ano = 1997
 
 
 -- Consulta 5
 -- Buscar os filmes lançados APÓS o ano 2000
-SELECT Nome, Ano, Duracao FROM Filmes
+SELECT Nome, Ano, Duracao 
+FROM Filmes
 WHERE Ano > 2000
 ORDER BY Ano
 
@@ -42,7 +47,8 @@ ORDER BY Ano
 -- Consulta 6
 -- Buscar os filmes com a duracao maior que 100 e menor que 150, 
 -- ordenando pela duracao em ordem crescente
-SELECT Nome, Ano, Duracao FROM Filmes
+SELECT Nome, Ano, Duracao 
+FROM Filmes
 WHERE Duracao > 100 AND Duracao < 150
 ORDER BY Duracao
 
@@ -51,14 +57,16 @@ ORDER BY Duracao
 -- Buscar a quantidade de filmes lançadas no ano, agrupando por ano,
 -- ordenando pela duracao em ordem decrescente
 -- OBS.: nao faz sentido ordenar pela duracao nessa consulta da maneira q o bootcamp pede
-SELECT Ano, COUNT(Ano) Quantidade FROM Filmes
+SELECT Ano, COUNT(Ano) Quantidade 
+FROM Filmes
 GROUP BY Ano
 ORDER BY Quantidade DESC
 
 
 -- Consulta 8
 -- Buscar os Atores do gênero masculino, retornando o PrimeiroNome, UltimoNome
-SELECT PrimeiroNome, UltimoNome, Genero FROM Atores
+SELECT PrimeiroNome, UltimoNome, Genero 
+FROM Atores
 WHERE Genero = 'M'
 ORDER BY PrimeiroNome, UltimoNome
 
@@ -66,6 +74,20 @@ ORDER BY PrimeiroNome, UltimoNome
 -- Consulta 9
 -- Buscar os Atores do gênero feminino, retornando o PrimeiroNome, UltimoNome,
 -- e ordenando pelo PrimeiroNome
-SELECT PrimeiroNome, UltimoNome, Genero FROM Atores
+SELECT PrimeiroNome, UltimoNome, Genero 
+FROM Atores
 WHERE Genero = 'F'
 ORDER BY PrimeiroNome
+
+
+-- Consulta 10
+-- Buscar o nome do filme e o gênero
+SELECT 
+	F.Nome,
+	G.Genero
+FROM 
+	FilmesGenero
+	INNER JOIN Filmes F ON FilmesGenero.IdFilme = F.Id
+	INNER JOIN Generos G ON FilmesGenero.IdGenero = G.Id
+ORDER BY 
+	G.Genero
