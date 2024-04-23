d538ca85c4cea0326c389c7ca1e58b0d1502a620
RafaelBP02 <rafaelbertopereira@gmail.com>
2024-04-23 17:47:17 -0300

    Cria consulta 1

---
+++ 12Consultas.sql
@@ -0,0 +1,14 @@
+-- \\\\\\\\\\\\\\\\\\\\\\\\\\\\
+-- CONSULTAS PARA BOOTCAMP DIO
+-- \\\\\\\\\\\\\\\\\\\\\\\\\\\\
+--
+-- Responsavel: Rafael Berto Pereira
+-- Versao: 1.0
+-- Ultima Modificacao: 23/04/2024
+--
+-- Resumo da ultima alteracao: Criou-se as 12 consultas requisitadas pelo desafio
+
+
+-- Consulta 1 
+-- Buscar o nome e ano dos filmes
+SELECT Nome, Ano FROM Filmes
